cf1d81103dc3b75a99a049a306271d0890494410
DekayKim <widoint@gmail.com>
2023-11-11 18:07:31 +0900

    UI: page 기본 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,117 @@
+'use client';
+
 import Image from 'next/image'
+import { useEffect, useState } from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card, CardContent, CardDescription,
+    CardFooter, CardHeader, CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+
+const MODEL_PRESET: Record<string, { url: string, libMap: string }> = {
+    '7b': {
+        url: 'https://huggingface.co/sionic-ai/chat-Llama-2-7b-chat-hf-q4f32_1/resolve/main/',
+        libMap: './model-lib/Llama-2-7b-chat-hf-q4f32_1-webgpu.wasm',
+    },
+    '13b': {
+        url: 'https://huggingface.co/sionic-ai/chat-Llama-2-13b-chat-hf-q4f32_1/resolve/main/',
+        libMap: './model-lib/Llama-2-13b-chat-hf-q4f32_1-webgpu.wasm',
+    },
+    '70b': {
+        url: 'https://huggingface.co/sionic-ai/chat-Llama-2-70b-chat-hf-q4f16_1/resolve/main/',
+        libMap: './model-lib/Llama-2-70b-chat-hf-q4f16_1-webgpu.wasm',
+    }
+};
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [selected, setSelected] = useState('7b');
+    const [modelUrl, setModelUrl] = useState('');
+    const [modelLibMap, setModelLibMap] = useState('');
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    useEffect(() => {
+        if (MODEL_PRESET[selected]) {
+            setModelUrl(MODEL_PRESET[selected].url);
+            setModelLibMap(MODEL_PRESET[selected].libMap);
+        } else {
+            setModelUrl('');
+            setModelLibMap('');
+        }
+    }, [selected]);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="p-9 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+                <h2 className="text-5xl font-bold tracking-tight">WASM LLM Loader</h2>
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Select Model</CardTitle>
+                        <CardDescription>
+                            Select a ready-made model or enter your own built model
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="grid gap-6">
+                        <RadioGroup defaultValue="card" className="grid grid-cols-4 gap-4">
+                            <div>
+                                <RadioGroupItem value="7b" id="7b" className="peer sr-only" onClick={e => setSelected(e.currentTarget.value)} />
+                                <Label
+                                    htmlFor="7b"
+                                    className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                                >
+                                    <Image src="/llama.png" alt="Llama2 7B Chat" width={180} height={37} />
+                                    Llama 2 7B Chat
+                                </Label>
+                            </div>
+                            <div>
+                                <RadioGroupItem value="13b" id="13b" className="peer sr-only" onClick={e => setSelected(e.currentTarget.value)} />
+                                <Label
+                                    htmlFor="13b"
+                                    className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                                >
+                                    <Image src="/llama.png" alt="Llama2 7B Chat" width={180} height={37} />
+                                    Llama 2 13B Chat
+                                </Label>
+                            </div>
+                            <div>
+                                <RadioGroupItem value="70b" id="70b" className="peer sr-only" onClick={e => setSelected(e.currentTarget.value)} />
+                                <Label
+                                    htmlFor="70b"
+                                    className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                                >
+                                    <Image src="/llama.png" alt="Llama2 7B Chat" width={180} height={37} />
+                                    Llama 2 70B Chat
+                                </Label>
+                            </div>
+                            <div>
+                                <RadioGroupItem value="custom" id="custom" className="peer sr-only" onClick={e => setSelected(e.currentTarget.value)} />
+                                <Label
+                                    htmlFor="custom"
+                                    className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                                >
+                                    <Image src="/wasm.png" alt="Custom Model" width={180} height={37} />
+                                    Custom Model
+                                </Label>
+                            </div>
+                        </RadioGroup>
+                        <div className="grid gap-2 mt-8">
+                            <Label htmlFor="model-url">Model URL</Label>
+                            <Input id="model-url" value={modelUrl} disabled={selected !== 'custom'} />
+                            <Label htmlFor="model-url">Model Lib Map</Label>
+                            <Input id="model-url" value={modelLibMap} disabled={selected !== 'custom'} />
+                        </div>
+                    </CardContent>
+                    <CardFooter>
+                        <Button className="w-full">Continue</Button>
+                    </CardFooter>
+                </Card>
+            </div>
+        </main>
+    )
 }
