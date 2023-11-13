2ca8b8d813fc9de1d988b89c93af598bacfb0512
DekayKim <widoint@gmail.com>
2023-11-14 02:24:16 +0900

    UI: index padding, flex

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -93,7 +93,7 @@ export default function Home() {
     }, [selected]);
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center px-24 py-8">
             <div className="p-9 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
                 <Image src="/webgpu.png" alt="Llama2 7B Chat" width={64} height={64} className="mr-2"/>
                 <h2 className="text-5xl font-bold tracking-tight">WebGPU LLM Loader</h2>
